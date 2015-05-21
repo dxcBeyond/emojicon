60dece044a18210f6f86c58ffad8db946a654a31
Vincent Deng <15815912517@163.com>
2015-05-21 13:23:51 +0800

    Vincent 更新了设置，没有修改任何代码

--- example/src/main/java/com/rockerhieu/emojicon/example/MainActivity.java
+++ example/src/main/java/com/rockerhieu/emojicon/example/MainActivity.java
@@ -19,48 +19,51 @@ package com.rockerhieu.emojicon.example;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 
 import com.rockerhieu.emojicon.EmojiconEditText;
 import com.rockerhieu.emojicon.EmojiconGridFragment;
-import com.rockerhieu.emojicon.EmojiconTextView;
 import com.rockerhieu.emojicon.EmojiconsFragment;
 import com.rockerhieu.emojicon.emoji.Emojicon;
 
 public class MainActivity extends FragmentActivity implements EmojiconGridFragment.OnEmojiconClickedListener, EmojiconsFragment.OnEmojiconBackspaceClickedListener {
+	/**
+	 * 可以输入的文本框
+	 */
     EmojiconEditText mEditEmojicon;
-    EmojiconTextView mTxtEmojicon;
-    CheckBox mCheckBox;
+    /**
+     * 仅仅用来显示的文本框
+     */
+//    EmojiconTextView mTxtEmojicon;
+//    CheckBox mCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mEditEmojicon = (EmojiconEditText) findViewById(R.id.editEmojicon);
-        mTxtEmojicon = (EmojiconTextView) findViewById(R.id.txtEmojicon);
-        mEditEmojicon.addTextChangedListener(new TextWatcherAdapter() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                mTxtEmojicon.setText(s);
-            }
-        });
-        mCheckBox = (CheckBox) findViewById(R.id.use_system_default);
-        mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                mEditEmojicon.setUseSystemDefault(b);
-                mTxtEmojicon.setUseSystemDefault(b);
-                setEmojiconFragment(b);
-            }
-        });
+//        mTxtEmojicon = (EmojiconTextView) findViewById(R.id.txtEmojicon);
+//        mEditEmojicon.addTextChangedListener(new TextWatcherAdapter() {
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                mTxtEmojicon.setText(s);
+//            }
+//        });
+//        mCheckBox = (CheckBox) findViewById(R.id.use_system_default);
+//        mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                mEditEmojicon.setUseSystemDefault(b);
+//                mTxtEmojicon.setUseSystemDefault(b);
+//                setEmojiconFragment(b);
+//            }
+//        });
 
+        
         setEmojiconFragment(false);
     }
 
     private void setEmojiconFragment(boolean useSystemDefault) {
-        getSupportFragmentManager()
-                .beginTransaction()
+        getSupportFragmentManager().beginTransaction()
                 .replace(R.id.emojicons, EmojiconsFragment.newInstance(useSystemDefault))
                 .commit();
     }
